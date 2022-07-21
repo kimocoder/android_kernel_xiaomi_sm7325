4d5a5d619632eec71c3597224fe0e088d467a20c
Divyanshu-Modi <divyan.m05@gmail.com>
2022-07-21 09:36:04 +0530

    drivers: remoteproc: Add support for remoteproc for yupik
    Values are verified using legacy dts nodes
    
    Change-Id: I61d9bea9c3aa92d54802fbf610f810ac0a8c7749
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -1014,6 +1014,20 @@ static const struct adsp_data parrot_adsp_resource = {
 	.ssctl_id = 0x14,
 };
 
+static const struct adsp_data yupik_adsp_resource = {
+	.crash_reason_smem = 423,
+	.firmware_name = "adsp.mdt",
+	.pas_id = 1,
+	.minidump_id = 5,
+	.uses_elf64 = true,
+	.has_aggre2_clk = false,
+	.auto_boot = false,
+	.ssr_name = "lpass",
+	.sysmon_name = "adsp",
+	.qmp_name = "adsp",
+	.ssctl_id = 0x14,
+};
+
 static const struct adsp_data msm8998_adsp_resource = {
 		.crash_reason_smem = 423,
 		.firmware_name = "adsp.mdt",
@@ -1162,6 +1176,20 @@ static const struct adsp_data parrot_cdsp_resource = {
 	.ssctl_id = 0x17,
 };
 
+static const struct adsp_data yupik_cdsp_resource = {
+	.crash_reason_smem = 601,
+	.firmware_name = "cdsp.mdt",
+	.pas_id = 18,
+	.minidump_id = 7,
+	.uses_elf64 = true,
+	.has_aggre2_clk = false,
+	.auto_boot = false,
+	.ssr_name = "cdsp",
+	.sysmon_name = "cdsp",
+	.qmp_name = "cdsp",
+	.ssctl_id = 0x17,
+};
+
 static const struct adsp_data mpss_resource_init = {
 	.crash_reason_smem = 421,
 	.firmware_name = "modem.mdt",
@@ -1231,6 +1259,22 @@ static const struct adsp_data cape_mpss_resource = {
 };
 
 static const struct adsp_data parrot_mpss_resource = {
+	.crash_reason_smem = 421,
+	.firmware_name = "modem.mdt",
+	.pas_id = 4,
+	.free_after_auth_reset = true,
+	.minidump_id = 3,
+	.uses_elf64 = true,
+	.has_aggre2_clk = false,
+	.auto_boot = false,
+	.ssr_name = "mpss",
+	.sysmon_name = "modem",
+	.qmp_name = "modem",
+	.ssctl_id = 0x12,
+	.dma_phys_below_32b = true,
+};
+
+static const struct adsp_data yupik_mpss_resource = {
 	.crash_reason_smem = 421,
 	.firmware_name = "modem.mdt",
 	.pas_id = 4,
@@ -1370,6 +1414,18 @@ static const struct adsp_data neo_wpss_resource = {
 	.ssctl_id = 0x19,
 };
 
+static const struct adsp_data yupik_wpss_resource = {
+	.crash_reason_smem = 626,
+	.firmware_name = "wpss.mdt",
+	.pas_id = 6,
+	.minidump_id = 4,
+	.uses_elf64 = true,
+	.ssr_name = "wpss",
+	.sysmon_name = "wpss",
+	.qmp_name = "wpss",
+	.ssctl_id = 0x19,
+};
+
 static const struct of_device_id adsp_of_match[] = {
 	{ .compatible = "qcom,msm8974-adsp-pil", .data = &adsp_resource_init},
 	{ .compatible = "qcom,msm8996-adsp-pil", .data = &adsp_resource_init},
@@ -1409,6 +1465,10 @@ static const struct of_device_id adsp_of_match[] = {
 	{ .compatible = "qcom,neo-wpss-pas", .data = &neo_wpss_resource},
 	{ .compatible = "qcom,anorak-adsp-pas", .data = &anorak_adsp_resource},
 	{ .compatible = "qcom,anorak-cdsp-pas", .data = &anorak_cdsp_resource},
+	{ .compatible = "qcom,yupik-adsp-pas", .data = &yupik_adsp_resource},
+	{ .compatible = "qcom,yupik-cdsp-pas", .data = &yupik_cdsp_resource},
+	{ .compatible = "qcom,yupik-modem-pas", .data = &yupik_mpss_resource},
+	{ .compatible = "qcom,yupik-wpss-pas", .data = &yupik_wpss_resource},
 	{ },
 };
 MODULE_DEVICE_TABLE(of, adsp_of_match);
