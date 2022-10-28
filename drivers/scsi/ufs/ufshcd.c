b73154a64650f7fe5ea1738b2876ed0cb0823a2b
Divyanshu-Modi <divyan.m05@gmail.com>
2022-10-28 10:43:55 +0530

    Revert "drivers: scsi: ufshcd: Show bogus clkscale_enable even if it's not supported"
    This reverts commit 59f6e20e9c91ea61e992cf5ebcdb2afea8df2d27.
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -1580,26 +1580,6 @@ static void ufshcd_resume_clkscaling(struct ufs_hba *hba)
 		devfreq_resume_device(hba->devfreq);
 }
 
-static int bogus_clkscale_enable = 1;
-static ssize_t ufshcd_bogus_clkscale_enable_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	return snprintf(buf, PAGE_SIZE, "%d\n", bogus_clkscale_enable);
-}
-
-static ssize_t ufshcd_bogus_clkscale_enable_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t count)
-{
-	u32 value;
-
-	if (kstrtou32(buf, 0, &value))
-		return -EINVAL;
-
-	bogus_clkscale_enable = !!value;
-
-	return count;
-}
-
 static ssize_t ufshcd_clkscale_enable_show(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
@@ -1646,15 +1626,10 @@ out:
 	return count;
 }
 
-static void ufshcd_clkscaling_init_sysfs(struct ufs_hba *hba, bool bogus)
+static void ufshcd_clkscaling_init_sysfs(struct ufs_hba *hba)
 {
-	if (bogus) {
-		hba->clk_scaling.enable_attr.show = ufshcd_bogus_clkscale_enable_show;
-		hba->clk_scaling.enable_attr.store = ufshcd_bogus_clkscale_enable_store;
-	} else {
-		hba->clk_scaling.enable_attr.show = ufshcd_clkscale_enable_show;
-		hba->clk_scaling.enable_attr.store = ufshcd_clkscale_enable_store;
-	}
+	hba->clk_scaling.enable_attr.show = ufshcd_clkscale_enable_show;
+	hba->clk_scaling.enable_attr.store = ufshcd_clkscale_enable_store;
 	sysfs_attr_init(&hba->clk_scaling.enable_attr.attr);
 	hba->clk_scaling.enable_attr.attr.name = "clkscale_enable";
 	hba->clk_scaling.enable_attr.attr.mode = 0644;
@@ -1964,7 +1939,8 @@ static void ufshcd_init_clk_scaling(struct ufs_hba *hba)
 {
 	char wq_name[sizeof("ufs_clkscaling_00")];
 
-	if (ufshcd_is_clkscaling_supported(hba)) {
+	if (!ufshcd_is_clkscaling_supported(hba))
+		return;
 
 	INIT_WORK(&hba->clk_scaling.suspend_work,
 		  ufshcd_clk_scaling_suspend_work);
@@ -1975,10 +1951,7 @@ static void ufshcd_init_clk_scaling(struct ufs_hba *hba)
 		 hba->host->host_no);
 	hba->clk_scaling.workq = create_singlethread_workqueue(wq_name);
 
-		ufshcd_clkscaling_init_sysfs(hba, false);
-	} else {
-		ufshcd_clkscaling_init_sysfs(hba, true);
-	}
+	ufshcd_clkscaling_init_sysfs(hba);
 }
 
 static void ufshcd_exit_clk_scaling(struct ufs_hba *hba)
